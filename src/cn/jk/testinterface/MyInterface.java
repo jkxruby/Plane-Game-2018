a877cbfc1163e7086ad119444d25b5497e929519
jkxruby <jkxruby@gmail.com>
2018-10-14 13:18:56 +0800

    抽象类和方法

---
+++ src/cn/jk/testinterface/MyInterface.java
@@ -0,0 +1,14 @@
+package cn.jk.testinterface;
+
+/**
+ * Created by jkx on 2018/10/3.
+ */
+public interface MyInterface {  // 除了把 class 换成 interface外，其他没多大区别
+    // 接口中只有常量和 抽象方法
+    String MAX_GREAD = "BOSS";
+    int MAX_SPEED = 120;
+    public void test01();
+    public void test02(int a, int b);
+
+
+}
